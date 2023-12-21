c710980cf1c3b6a02c87db4bb1b3b0334fede1c6
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-22 02:15:45 +0500

    loop to check elements and take input for range

--- array_utils.c
+++ array_utils.c
@@ -4,9 +4,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int contains(const *arr,int size,intx){
+int cont(const * arr,int size,int x){
     if(size<=0){
         printf("invalid input\n");
         return 0;
+
     }
+    int check=0;
+    // loop to check element
+    for(int i=0;i<size;i++){
+        if(arr[i]==x){
+         check++;
+         break;
+        }    
+}
+if(check !=0){
+    return 1;
+}
+else{
+    return 0;
+}
+    
 }
