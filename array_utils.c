ba8ca9badccd6ebd4120b4320fd55a37a3320b96
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-22 02:28:41 +0500

    check elements in loop and condition

--- array_utils.c
+++ array_utils.c
@@ -24,5 +24,31 @@ if(check !=0){
 else{
     return 0;
 }
-    
+
+}
+ int containsWithin(const int *arr, int size, int x, int i, int j){
+  
+    if(size<=0||i<0||j>size){
+        printf("Invalide input to function...");
+        return 0;
+    }
+    int check=0;
+    for(int index=i;index<j;index++){
+        if(arr[index]==x){
+            check++;
+            break;
+        }
+    }
+    if(check==1){
+        return\ 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int * paddedCopy(const int *arr, int oldSize, int newSize){
+    if(arr==NULL||oldsize<=0||newsize<=0){
+        printf("Invalide Input to function...\n");
+        return NULL;}
 }
