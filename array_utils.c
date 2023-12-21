ff8da6ed06d695f8c736b98b95e2a0db9fe0980a
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-22 01:52:51 +0500

    dynamic,reverse arr,and ther prototypes

--- array_utils.c
+++ array_utils.c
@@ -1,3 +1,12 @@
 
 
 //place your function definitions here
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int contains(const *arr,int size,intx){
+    if(size<=0){
+        printf("invalid input\n");
+        return 0;
+    }
+}
--- array_utils.h
+++ array_utils.h
@@ -1,3 +1,12 @@
 
 
 //place your documentation and prototypes here
+int conts(const int *arr,int size,int x);
+int contswithin(const int *arr,int size,int x,int i ,int j);
+//dynamic array
+int *paddedcopy(const int*arr,int oldsize,int newsize);
+//print array
+void printarray(int*arr,int oldsize,int newsize);
+//reverse array
+void reverse(int *arr,int size);
+int*reversecopy(int*arr,int size);
